1f83bdc5a1e01fb4bf8d5efe8b08a9bdfc4c5995
thehannk <108367955+thehannk@users.noreply.github.com>
2022-07-14 03:15:00 +0300

    Kullanıcıdan alınan sayı tek mi yoksa çift mi?

---
+++ CiftMi-TekMi-Metot/CiftMi-TekMi-Metot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiftMi_TekMi_Metot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Birinci sayıyı giriniz :");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İkinci sayıyı giriniz :");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            kontrolEt(sayi1, sayi2);
+            Console.ReadLine();
+
+        }
+
+        static void kontrolEt(int a, int b)
+        {
+            if (a > b)
+            {
+                Console.WriteLine("Birinci sayı ikinci sayıdan büyüktür.");
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("İki sayı da birbirine eşittir.");
+            }
+            else
+            {
+                Console.WriteLine("Birinci sayı ikinci sayıdan küçüktür.");
+            }
+        }
+
+    }
+}
